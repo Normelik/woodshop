96c597e58d7a891cf9cac326d4c1c3a7b0ab1dd4
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-10-09 17:33:11 +0200

    added default user

--- backend/src/main/java/com/wood/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/wood/backend/config/SecurityConfig.java
@@ -2,9 +2,19 @@ package com.wood.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +26,9 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(
                         auth -> {
-                            auth.anyRequest().authenticated();
+                            auth
+                                    .requestMatchers("/login").permitAll()
+                                    .anyRequest().authenticated();
                         }
                 )
                 .httpBasic(Customizer.withDefaults())
@@ -24,4 +36,30 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) throws Exception {
+
+            DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+            authenticationProvider.setUserDetailsService(userDetailsService);
+            authenticationProvider.setPasswordEncoder(passwordEncoder);
+            return new ProviderManager(authenticationProvider);
+    }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user1")
+                        .password("password1")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
